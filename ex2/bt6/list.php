eef98c0f1347d6e0ccabf0620eff548d276222be
HuynhTien252~ <huynhtienadus252@gmail.com>
2025-08-17 01:03:13 +0700

    Create list.php

---
+++ ex2/bt6/list.php
@@ -0,0 +1,20 @@
+<?php
+// Khai báo mảng sản phẩm
+$products = [
+    [
+        "name" => "Laptop Dell XPS",
+        "price" => 25000000,
+        "description" => "Laptop cao cấp, màn hình 4K, chip Intel i7"
+    ],
+    [
+        "name" => "Chuột Logitech",
+        "price" => 450000,
+        "description" => "Chuột không dây, pin lâu, thiết kế ergonomic"
+    ],
+    [
+        "name" => "Tai nghe Sony WH-1000XM5",
+        "price" => 6990000,
+        "description" => "Tai nghe chống ồn chủ động, âm thanh chất lượng cao"
+    ]
+];
+?>

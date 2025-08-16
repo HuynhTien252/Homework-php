e7b0a7fb0c9c992e2455c797285256f837c0bc75
HuynhTien252~ <huynhtienadus252@gmail.com>
2025-08-17 01:02:54 +0700

    Create bt5.php

---
+++ ex1/bt5.php
@@ -0,0 +1,27 @@
+<?php
+$a = null;
+$b = false;
+$c = 1;
+$d = 2.5;
+$e = "How are you";
+$arr = [
+    "1"=>2,
+    "2"=>4,
+    "3"=>6];
+
+echo $a . "\n";
+
+echo $b . "\n";  
+
+echo $c . "\n";
+
+echo $d . "\n";
+
+echo $e . "\n";
+
+
+echo $arr[1] . PHP_EOL;
+echo $arr[2] . PHP_EOL;
+echo $arr[3] . PHP_EOL;
+
+

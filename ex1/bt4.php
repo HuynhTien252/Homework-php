18583571ff8e8ec20483a1882f896eec81e36069
HuynhTien252~ <huynhtienadus252@gmail.com>
2025-08-17 01:02:53 +0700

    Create bt4.php

---
+++ ex1/bt4.php
@@ -0,0 +1,8 @@
+<?php
+/* Trong một file chỉ php mà không có html t có thể khai báo theo kiểu như sau:
+    1. <?php $demo = "Hello"; ?> với kiểu khai báo này khi kết thúc câu lệnh dấu ( ; ) có cũng hoặc không có cũng được
+    2. <?php $demo = "Hello"; với kiểu khai báo không có đuôi ( ?> ) thì khi kết thúc câu lệnh bắt buộc phải có dấu ( ; )
+ */
+$message = "PHP Comments"; // dấu ( $ ) trong PHP dùng để khai báo biến
+
+echo $message; # echo là câu lệnh dùng để in ra giá trị muốn hiển thị 
